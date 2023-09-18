ee39909c8ff18e71d038e3fe785ba18672dfa089
Roger Sepulveda <rseplved@starbucks.com>
2023-09-18 16:34:15 -0500

    First exercise

---
+++ test_calculator.py
@@ -0,0 +1,12 @@
+import unittest
+from calculator import suma
+
+class TestCalculator(unittest.TestCase):
+
+    def test_suma(self):
+        self.assertEqual(suma(2, 3), 5)
+        self.assertEqual(suma(-1, 1), 0)
+        self.assertEqual(suma(-1, -1), -2)
+
+if __name__ == "__main__":
+    unittest.main()
